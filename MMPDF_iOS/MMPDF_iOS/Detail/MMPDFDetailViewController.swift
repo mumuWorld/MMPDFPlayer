2765e01129243e462c29e54775682b8002ba2202
mumu <617958070@qq.com>
2021-08-01 23:58:50 +0800

    mod: pdf

--- MMPDF_iOS/MMPDF_iOS/Detail/MMPDFDetailViewController.swift
+++ MMPDF_iOS/MMPDF_iOS/Detail/MMPDFDetailViewController.swift
@@ -8,7 +8,9 @@
 import UIKit
 
 class MMPDFDetailViewController: MMBaseViewController {
-
+    
+    @IBOutlet weak var bottomToolView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .red
