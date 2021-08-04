5eafeb63f3a631b516f79d7b0916cac823c90ac8
mumu <617958070@qq.com>
2021-08-04 23:44:58 +0800

    add: 展示pdf

--- MMPDF_iOS/MMPDF_iOS/Detail/MMPDFDetailViewController.swift
+++ MMPDF_iOS/MMPDF_iOS/Detail/MMPDFDetailViewController.swift
@@ -6,14 +6,33 @@
 //
 
 import UIKit
+import PDFKit
 
 class MMPDFDetailViewController: MMBaseViewController {
     
     @IBOutlet weak var bottomToolView: UIView!
     
+    lazy var pdfView: PDFView = {
+        let item = PDFView()
+        item.autoScales = true
+        item.isUserInteractionEnabled = true
+        return item
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        containerView.backgroundColor = .red
+        
+        let path = Bundle.main.path(forResource: "Swift 开发者必备 Tips (第四版)", ofType: "pdf") ?? ""
+        let url = URL(fileURLWithPath: path)
+        let document = PDFDocument(url: url)
+        
+        containerView.addSubview(pdfView)
+        pdfView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        
+        pdfView.document = document
     }
 
     
